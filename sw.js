06d3c002e697354b037cec89130765ad563ccf93
Audrey Tang <audreyt@audreyt.org>
2020-02-08 18:40:44 +0800

    * make this a PWA.

---
+++ sw.js
@@ -0,0 +1,36 @@
+self.addEventListener('install', function(event) {
+
+// Perform install steps
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  'stylesheet/bootstrap.uncss.css',
+  'stylesheet/style.css'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+});
